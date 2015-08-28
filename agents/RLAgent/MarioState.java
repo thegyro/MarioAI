ccee9161bf607d8867851f728d5c10d56886a618
Srinath R <srinath132@gmail.com>
2015-08-28 16:22:03 +0530

    First commit. Inaug.

---
+++ agents/RLAgent/MarioState.java
@@ -0,0 +1,13 @@
+package myagent.agents.RLAgent;
+
+import ch.idsia.benchmark.mario.environments.Environment;
+
+
+class MarioState {
+
+private byte[][] levelScene;
+private byte[][] enemies;
+
+
+
+}
